2a929457eb6ad7d9e7dbdd58ed30fa73631c0150
Azure <kvnliwag@gmail.com>
2016-01-13 08:07:45 -0800

    Removed table and reference

--- azuredb-create.sql
+++ azuredb-create.sql
@@ -24,15 +24,5 @@ CREATE TABLE skills (
 	skillName VARCHAR(140),
 	skillCategory INT NOT NULL,
 	skillStrength VARCHAR(140) NOT NULL,
-	PRIMARY KEY (ID);
+	PRIMARY KEY (ID)
 );
-
-CREATE TABLE skillCategories (
-	ID INT NOT NULL IDENTITY(1,1),
-	skillCategoriesName VARCHAR(140) NOT NULL,
-	skillStrengthLevel DECIMAL(3,2) CONSTRAINT chk_skillStrLvl check (skillStrengthLevel between 0 and 1),
-);
-
-ALTER TABLE skills WITH CHECK
-	ADD CONSTRAINT [FK_Prod_Man] FOREIGN KEY (skillCategory)
-	REFERENCES skillCategories(skillCategoriesName);
